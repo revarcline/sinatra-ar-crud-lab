9e793e0b86c27d1ba3231e38f845338e17ef50d7
Alex Cline <rev.a.r.cline@gmail.com>
2021-01-23 13:44:21 -0600

    Done.

--- Gemfile
+++ Gemfile
@@ -1,20 +1,19 @@
 source 'http://rubygems.org'
 
 gem 'activerecord', '~> 5.2.3', require: 'active_record'
-gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+gem 'sinatra-activerecord', require: 'sinatra/activerecord'
 
-gem 'sinatra'
-gem 'pry-nav'
-gem 'rake'
-gem 'rspec'
-gem 'rack-test'
 gem 'database_cleaner', '~> 1.6.0'
+gem 'pry-nav'
+gem 'rack-test'
+gem 'rake'
 gem 'require_all'
-
+gem 'rspec'
+gem 'sinatra'
 
 group :development do
-  gem "capybara"
-  gem "pry"
+  gem 'capybara'
+  gem 'pry'
+  gem 'shotgun'
   gem 'sqlite3', '~> 1.3.6'
-  gem "shotgun"
 end
--- config/environment.rb
+++ config/environment.rb
@@ -1,19 +1,18 @@
-ENV['SINATRA_ENV'] ||= "development"
+ENV['SINATRA_ENV'] ||= 'development'
 
 require 'bundler/setup'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 def fi_check_migration
-  begin
-    ActiveRecord::Migration.check_pending!
-  rescue ActiveRecord::PendingMigrationError
-    raise ActiveRecord::PendingMigrationError.new "Migrations are pending.\nTo resolve this issue, run: \nrake db:migrate SINATRA_ENV=#{ENV['SINATRA_ENV']}"
-  end
+  ActiveRecord::Migration.check_pending!
+rescue ActiveRecord::PendingMigrationError
+  raise ActiveRecord::PendingMigrationError,
+        "Migrations are pending.\nTo resolve this issue, run: \nrake db:migrate SINATRA_ENV=#{ENV['SINATRA_ENV']}"
 end
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  adapter: 'sqlite3',
+  database: "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 
 require_all 'app'
